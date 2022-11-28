262cc8836aa96b795869ae7848a7e2edf7eb9057
Andrea Calix <calixandrea25@gmail.com>
2022-11-27 19:41:41 -0600

    el lenguaje de programacion ya puede realizar operaciones +-/* y detectar errores ej, div por cero

--- Lenguaje_Programacion/basic.py
+++ Lenguaje_Programacion/basic.py
@@ -24,7 +24,29 @@ class IllegalCharError(Error):
 class InvalidSyntaxError(Error):
 		def __init__(self, pos_start, pos_end, details=''):
 				super().__init__(pos_start, pos_end, 'Sintaxis Invalida', details)
-
+# errores de division por cero
+class RTError(Error):
+		def __init__(self, pos_start, pos_end, details, context):
+				super().__init__(pos_start, pos_end, 'Error de ejecucion', details)
+				self.context = context
+		
+		def as_string(self):
+			result = self.generate_traceback() 
+			result += f'{self.error_name}: {self.details}\n'
+			result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+			return result
+		
+		def generate_traceback(self):
+			result = ''
+			pos = self.pos_start
+			ctx = self.context
+			
+			while ctx:
+				result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+				pos = ctx.parent_entry_pos
+				ctx = ctx.parent
+			
+			return 'Traceback(llamada mas reciente):\n' + result
 
 # POSITION
 class Position:
@@ -149,12 +171,15 @@ class Lexer:
 						return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
 
 #######################################
-# NODES
+# NODOS
 #######################################
 
 class NumberNode:
 	def __init__(self, tok):
 		self.tok = tok
+		
+		self.pos_start = self.tok.pos_start
+		self.pos_end = self.tok.pos_end
 
 	def __repr__(self):
 		return f'{self.tok}'
@@ -164,6 +189,9 @@ class BinOpNode:
 		self.left_node = left_node
 		self.op_tok = op_tok
 		self.right_node = right_node
+		
+		self.pos_start = self.left_node.pos_start
+		self.pos_end = self.right_node.pos_end
 
 	def __repr__(self):
 		return f'({self.left_node}, {self.op_tok}, {self.right_node})'
@@ -172,6 +200,9 @@ class UnaryOpNode:
 	def __init__(self, op_tok, node):
 		self.op_tok = op_tok
 		self.node = node
+		
+		self.pos_start = self.op_tok.pos_start
+		self.pos_end = node.pos_end
 
 	def __repr__(self):
 		return f'({self.op_tok}, {self.node})'
@@ -274,7 +305,133 @@ class Parser:
 			left = BinOpNode(left, op_tok, right)
 
 		return res.success(left)
+#######################################
+# TIEMPO DE EJECUCION
+#######################################		
+class RTResult:
+	def __init__(self):
+		self.value = None
+		self.error = None
+		
+	def registrer (self, res):
+		if res.error: self.error = res.error
+		return res.value
+	def success(self, value):
+		self.value = value
+		return self
+	def failure(self, error):
+		self.error = error
+		return self
+		
+#######################################
+# EVALUAR NUMEROS
+#######################################	
+class Number:
+	def __init__(self, value):
+		self.value = value
+		self.set_pos()
+		self.set_context()
+		
+	def set_pos(self, pos_start = None, pos_end = None): #funcion para saber la posicion en caso de que llegue a existir una division por cero se detecta la posicion del error 
+		self.pos_start = pos_start
+		self.pos_end = pos_end
+		return self
+	
+	def set_context(self, context = None):
+		self.context = context
+		return self
+		
+		
+	## metodos para operar los numeros
+	#adicion
+	def added_to(self, other):
+		if isinstance(other, Number): #verificar que el dato recibido es un numero
+			return Number(self.value + other.value).set_context(self.context), None
+	# sustraccion
+	def subbed_by(self, other):
+		if isinstance(other, Number): 
+			return Number(self.value - other.value).set_context(self.context), None
+	# multiplicacion
+	def multed_by(self, other):
+		if isinstance(other, Number): #verificar que el dato recibido es un numero
+			return Number(self.value * other.value).set_context(self.context), None
+	
+	# division
+	def dived_by(self, other):
+		if isinstance(other, Number): #verificar que el dato recibido es un numero
+			if other.value == 0:
+				return None, RTError(other.pos_start, other.pos_end,'Division por cero', self.context) 
+			
+			return Number(self.value / other.value).set_context(self.context), None
+		
+	def __repr__(self):
+		return str(self.value)
 
+#######################################
+# CONTEXTO
+#######################################	
+# el objtivo de esta clase es detectar cuando se esta trabajando con una funcion y cuando se esta trabajando en todo el programa
+class Context:
+	def __init__(self, display_name, parent = None, parent_entry_pos = None):
+		self.display_name = display_name
+		self.parent = parent
+		self.parent_entry_pos = parent_entry_pos
+	
+#######################################
+# INTERPRETE
+#######################################
+	
+#crear una clase interprete 
+class Interpreter:
+	def visit(self, node, context):
+		method_name = f'visit_{type(node).__name__}'
+		method = getattr(self, method_name, self.no_visit_method)
+		return method(node, context)
+	
+	def no_visit_method(self, node, context):
+		raise Exception(f'No visit_{type(node).__name__} method defined')
+	
+	def visit_NumberNode(self, node, context):
+		return RTResult().success(
+	    Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end))
+    
+	def visit_BinOpNode(self, node, context):
+		res = RTResult()
+		left = res.registrer(self.visit(node.left_node, context))
+		if res.error: return res
+		right= res.registrer(self.visit(node.right_node, context))
+		if res.error: return res
+		
+		if node.op_tok.type ==TT_SUMA:
+			result, error = left.added_to(right)
+			
+		elif node.op_tok.type ==TT_RESTA:
+			result, error = left.subbed_by(right)
+			
+		elif node.op_tok.type ==TT_MUL:
+			result, error = left.multed_by(right)
+			
+		elif node.op_tok.type ==TT_DIV:
+			result, error = left.dived_by(right)
+		
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(result.set_pos(node.pos_start, node.pos_end))
+	
+	def visit_UnaryOpNode(self, node, context):
+		res = RTResult()	
+		number = res.registrer(self.visit(node.node, context))
+		if res.error: return res
+		error = None
+		if node.op_tok.type == TT_RESTA:
+			number, error = number.multed_by(Number(-1))
+		
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(number.set_pos(node.pos_start, node.pos_end))
+	
 #######################################
 # CORRER
 #######################################
@@ -288,5 +445,11 @@ def run(fn, text):
 		# generar arbol sintactico
 		parser = Parser(tokens)
 		ast = parser.parse()
+		if ast.error: return None, ast.error
+		
+		#correr el interprete 
+		interpreter = Interpreter()
+		context = Context('<programa>')
+		result = interpreter.visit(ast.node, context)
 
-		return ast.node, ast.error
+		return  result.value, result.error
